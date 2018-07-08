fc0fc7ea861d262f776d594dcc020a125ac45289
Curtis Mattoon <cmattoon@cmattoon.com>
2018-07-08 18:54:54 -0400

    (save)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,12 +10,13 @@ type Config struct {
 	Provider             string
 }
 
+
 func DefaultConfig() *Config {
 	cfg := &Config{
 		AWSRegion:            "us-west-2",
 		Interval:             30,
-		KubeConfig:           "",
-		KubeMaster:           "",
+		KubeConfig:           "/Users/cmattoon/.kube/config",
+		KubeMaster:           "https://api.dev-apps.us-west-2.k8s.entic-int.com",
 		MetricsListenAddress: "127.0.0.1:9999",
 		Provider:             "aws",
 	}
