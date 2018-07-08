75caa018677ea42884673510461f3b116d3b5481
Curtis Mattoon <cmattoon@cmattoon.com>
2018-07-08 15:43:51 -0400

    Save

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -6,25 +6,40 @@ import (
 	
 	log "github.com/sirupsen/logrus"
 	"github.com/cmattoon/aws-ssm/pkg/config"
+	"github.com/cmattoon/aws-ssm/pkg/provider"
 )
 
 
 type Controller struct {
 	Interval time.Duration
+	Provider provider.Provider
 }
 
 
 func NewController(cfg *config.Config) Controller {
+	p, err := provider.NewProvider(cfg)
+	if err != nil {
+		log.Fatalf("Failed to create provider: %s", err)
+	}
+	
 	ctrl := Controller{
 		Interval: time.Duration(cfg.Interval) * time.Second,
+		Provider: p,
 	}
+	
 	return ctrl
 }
 
 func (c *Controller) RunOnce() error {
 	log.Info("Running...")
-
+	name := "com.entic.foo"
+	decrypt := true
 	
+	val, err := c.Provider.GetParameterValue(name, decrypt)
+	if err != nil {
+		log.Fatalf("Failed to get value: %s", err)
+	}
+	log.Infof("Got value %s", val)
 	return nil
 }
 
