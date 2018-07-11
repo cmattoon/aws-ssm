4a0335c73f5226011d33ece68c8b7171bde54688
Curtis Mattoon <cmattoon@cmattoon.com>
2018-07-10 23:29:10 -0400

    Working-ish

--- pkg/annotations/annotations.go
+++ pkg/annotations/annotations.go
@@ -1,10 +1,10 @@
 package annotations
 
 const (
-	K8sSecretName = "alpha.ssm.cmattoon.com/k8s-secret-name"
-	K8sSecretType = "alpha.ssm.cmattoon.com/k8s-secret-type"
-	AwsParamName = "alpha.ssm.cmattoon.com/aws-param-name"
-	AwsParamType = "alpha.ssm.cmattoon.com/aws-param-type"
-	AwsParamKey = "alpha.ssm.cmattoon.com/aws-param-key"
+	K8SSecretName = "alpha.ssm.cmattoon.com/k8s-secret-name"
+	K8SSecretType = "alpha.ssm.cmattoon.com/k8s-secret-type"
+	AWSParamName = "alpha.ssm.cmattoon.com/aws-param-name"
+	AWSParamType = "alpha.ssm.cmattoon.com/aws-param-type"
+	AWSParamKey = "alpha.ssm.cmattoon.com/aws-param-key"
 )
 
