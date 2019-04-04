fef88735be2becae47ed3009d5a74fbc3413ab4b
Salma Suliman <ssuliman91@gmail.com>
2019-04-04 05:43:19 -0700

    Remove getting AWS credentials from env, and use the recommended default credential provider chain (#22)

--- pkg/provider/aws.go
+++ pkg/provider/aws.go
@@ -19,7 +19,6 @@ import (
 	"path"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/cmattoon/aws-ssm/pkg/config"
@@ -33,8 +32,7 @@ type AWSProvider struct {
 
 func NewAWSProvider(cfg *config.Config) (Provider, error) {
 	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(cfg.AWSRegion),
-		Credentials: credentials.NewEnvCredentials(),
+		Region: aws.String(cfg.AWSRegion),
 	})
 
 	if err != nil {
